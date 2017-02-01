e174e2ec514e2d4101410b98ee9f9a7aebfd440c
Guillaume CHAUVET <gchauvet@users.noreply.github.com>
2017-02-01 16:44:28 +0100

    Set default version values to avoid error in ResEdit

--- src/native/windows/apps/prunsrv/prunsrv.h
+++ src/native/windows/apps/prunsrv/prunsrv.h
@@ -27,7 +27,9 @@
 #define PRG_REGROOT   L"Apache Software Foundation\\Procrun 2.0"
 
 #if !defined(PRG_VERSION_MAJOR) || !defined(PRG_VERSION_MINOR) || !defined(PRG_VERSION_PATCH)
-#error "Version numbers undefined"
+#define PRG_VERSION_MAJOR 0
+#define PRG_VERSION_MINOR 0
+#define PRG_VERSION_PATCH 0
 #endif
 
 #endif /* _PRUNSRV_H */
