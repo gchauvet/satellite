23c58dff28c2c6c8ef2cdc2fb0bf3ebbc2d1c13c
Guillaume CHAUVET <gchauvet@users.noreply.github.com>
2017-04-10 16:33:29 +0200

    Add thanks

--- frontends/phobos/src/main/c/handles.c
+++ frontends/phobos/src/main/c/handles.c
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Contributed by Guillaume Chauvet <gchauvet@zatarox.com> 08 Apr 2017
  */
  
 #include "apxwin.h"
